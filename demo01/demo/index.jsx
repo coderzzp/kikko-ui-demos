18dffdabbc740b0a2e9e9e5dd6c051b9dbe8d068
zhipeng.zheng01 <zhipeng.zheng01@liulishuo.com>
2021-02-19 16:25:47 +0800

    feat: demo01

---
+++ demo01/demo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Button from '../src/index';
+
+function Demo() {
+  
+  return (
+    <Button word={123}></Button>
+  );
+}
+
+ReactDom.render(<Demo />, document.getElementById('root'));
---
+++ demo01/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './index.less'
+
+const Button = ()=>{
+    return <div className="kikko-button">im a button</div>
+}
+export default Button
