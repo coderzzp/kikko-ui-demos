0d31f2fe194bb7357bc9d96ec58178ad53414475
zhipeng.zheng01 <zhipeng.zheng01@liulishuo.com>
2021-02-20 10:56:32 +0800

    feat: add demo02

---
+++ demo03/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classnames from 'classnames'
+import './index.less'
+console.log('classnames',classnames)
+const Button = ()=>{
+    return <div className="kikko-button">im a button</div>
+}
+export default Button
